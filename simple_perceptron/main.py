527b6d234aa59d9fd0d5f64a496a02953dfe552d
Himeno59 <tomoya.himeno0428@gmail.com>
2025-04-17 14:20:00 +0900

    Implement the basic framework of a simple perceptron

--- simple_perceptron/main.py
+++ simple_perceptron/main.py
@@ -1,12 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from simple_perceptron import * # 同じ階層にある前提
+from sklearn.datasets import load_iris
 
 def main():
-    x = np.array([1,2,3])
-    w = np.array([1,2,3])
-    b = 1
-    perceptron = SimplePerceptron(x,w,b)
+    ### データ ###
+    # irisデータセット
+    iris = load_iris()
+    X = iris.data[:100, :2]
+    T = iris.target[:100]
 
-    perceptron.calc()
+    ### インスタンス生成 ###
+    w = np.zeros(2)
+    b = 0
+    perceptron = perceptron = SimplePerceptron(len(X[0]),w,b) 
+
+    ### 学習 ###
+    for epoch in range(10):
+        for x,t in zip(X,T): 
+            perceptron.train(x,t)
+
+    ## visualize ##
+    for i in range(len(X)):
+        if T[i] == 1:
+            plt.scatter(X[i][0], X[i][1], color='blue', label='Class 1' if i==0 else "")
+        else:
+            plt.scatter(X[i][0], X[i][1], color='red', label='Class 0' if i==2 else "")
+            
+    # 分離直線を描画
+    x1_vals = np.linspace(-3, 3, 100)
+    if w[1] != 0:
+        x2_vals = -(w[0] * x1_vals + b) / w[1]
+        plt.plot(x1_vals, x2_vals, color='green', label='Decision Boundary')
+    else:
+        plt.axvline(-b/w[0], color='green', label='Decision Boundary')
+        
+    plt.xlabel("x1")
+    plt.ylabel("x2")
+    plt.legend()
+    plt.grid(True)
+    plt.title("Perceptron Classification Result")
+    plt.show()
     
 if __name__ == "__main__":
     main()
--- simple_perceptron/simple_perceptron.py
+++ simple_perceptron/simple_perceptron.py
@@ -4,26 +4,19 @@ def sgn(x):
     return 1 if x >= 0 else 0
 
 class SimplePerceptron:
-    def __init__(self, x, w, b):
-        # check value
-        if not (isinstance(x, np.ndarray) and isinstance(w, np.ndarray)):
-            raise TypeError("xとwはNumPy配列である必要があります。")
-        if x.ndim != 1 or w.ndim != 1:
-            raise ValueError("xとwは1次元のベクトルでなければなりません。")
-        if len(x) != len(w):
-            raise ValueError("xとwは同じ長さでなければなりません。")
-        self.x = x
-        self.w = w
-        self.b = b
-
-        self.output = None
+    def __init__(self, input_size, w=None, b=0):
         
+        if not isinstance(w, np.ndarray):
+            raise TypeError("wはNumPy配列である必要があります。")
+    
+        self.w = w if w is not None else np.zeros(input_size)
+        self.b = b
+        self.y = None
+        
+    def calc(self, x):
+        return sgn(np.dot(self.w,x)+self.b)
 
-    def calc(self):
-        self.output = np.dot(self.w,self.x)+self.b
-        print(self.output)
-        return self.output
-
-    def predict(self):
-        print(sgn(self.output))
-        return sgn(self.output)
+    def train(self, x, t):
+        self.y = self.calc(x)
+        self.w += (t - self.y)*x
+        self.b += (t - self.y)
