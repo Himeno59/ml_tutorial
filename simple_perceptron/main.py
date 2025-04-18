25067761d3c208ed66050ce9ba19765d37e599a7
Himeno59 <tomoya.himeno0428@gmail.com>
2025-04-18 13:54:34 +0900

    Implemented a simple perceptron for binary classification using the Iris dataset

--- simple_perceptron/main.py
+++ simple_perceptron/main.py
@@ -1,46 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from simple_perceptron import * # 同じ階層にある前提
+from simple_perceptron import *
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
-def main():
-    ### データ ###
-    # irisデータセット
-    iris = load_iris()
-    X = iris.data[:100, :2]
-    T = iris.target[:100]
+## data ##
+iris = load_iris()
+print(f"data: ({iris.data.shape[0]}, {iris.data.shape[1]})")
 
-    ### インスタンス生成 ###
-    w = np.zeros(2)
-    b = 0
-    perceptron = perceptron = SimplePerceptron(len(X[0]),w,b) 
+## 線形分離のために最初の2種類を取り出す ##
+X = iris.data[:100,:]
+T = iris.target[:100]
+X_train, X_test, T_train, T_test = train_test_split(X,T,test_size=0.3,random_state=42, stratify=T)
+print(f"X: ({X.shape[0]}, {X.shape[1]})")
+print(f"T: ({len(T)})")
 
-    ### 学習 ###
-    for epoch in range(10):
-        for x,t in zip(X,T): 
-            perceptron.train(x,t)
-
-    ## visualize ##
-    for i in range(len(X)):
-        if T[i] == 1:
-            plt.scatter(X[i][0], X[i][1], color='blue', label='Class 1' if i==0 else "")
-        else:
-            plt.scatter(X[i][0], X[i][1], color='red', label='Class 0' if i==2 else "")
-            
-    # 分離直線を描画
-    x1_vals = np.linspace(-3, 3, 100)
-    if w[1] != 0:
-        x2_vals = -(w[0] * x1_vals + b) / w[1]
-        plt.plot(x1_vals, x2_vals, color='green', label='Decision Boundary')
-    else:
-        plt.axvline(-b/w[0], color='green', label='Decision Boundary')
-        
-    plt.xlabel("x1")
-    plt.ylabel("x2")
-    plt.legend()
-    plt.grid(True)
-    plt.title("Perceptron Classification Result")
-    plt.show()
+## param ##
+w = np.zeros(X.data.shape[1])
+b = 0
+lr = 0.1
+print(f"w: {w}, b: {b}, lr: {lr}")
     
-if __name__ == "__main__":
-    main()
+## create instance ##
+perceptron = perceptron = SimplePerceptron(X.shape[1],w,b,lr)
+
+## learning ##
+for epoch in range(1):
+    for x,t in zip(X_train,T_train): 
+        perceptron.train(x,t)
+        
+## test ##
+accuracy = perceptron.test(X_test, T_test)
+print("accuracy: ",accuracy)
+            
+# ## visualize ##
+# ## 特徴量が2次元で直線で書ける場合のみ
+# for i in range(len(X)):
+#     if T[i] == 1:
+#         plt.scatter(X[i][0], X[i][1], color='blue', label='Class 1' if i==0 else "")
+#     else:
+#         plt.scatter(X[i][0], X[i][1], color='red', label='Class 0' if i==2 else "")
+
+# plt.xlim(auto=True)
+# xlim = plt.xlim()
+# x1_vals = np.linspace(xlim[0], xlim[1], 100)
+# if w[1] != 0:
+#     x2_vals = -(w[0] * x1_vals + b) / w[1]
+#     plt.plot(x1_vals, x2_vals, color='green', label='Decision Boundary')
+# else:
+#     plt.axvline(-b/w[0], color='green', label='Decision Boundary')
+
+# plt.xlabel("x1")
+# plt.ylabel("x2")
+# plt.legend()
+# plt.grid(True)
+# plt.title("Perceptron Classification Result")
+# plt.show()    
--- simple_perceptron/simple_perceptron.py
+++ simple_perceptron/simple_perceptron.py
@@ -4,13 +4,14 @@ def sgn(x):
     return 1 if x >= 0 else 0
 
 class SimplePerceptron:
-    def __init__(self, input_size, w=None, b=0):
+    def __init__(self, input_size, w=None, b=0, lr=0.1):
         
         if not isinstance(w, np.ndarray):
             raise TypeError("wはNumPy配列である必要があります。")
     
         self.w = w if w is not None else np.zeros(input_size)
         self.b = b
+        self.lr = lr
         self.y = None
         
     def calc(self, x):
@@ -18,5 +19,13 @@ class SimplePerceptron:
 
     def train(self, x, t):
         self.y = self.calc(x)
-        self.w += (t - self.y)*x
-        self.b += (t - self.y)
+        self.w += self.lr * (t - self.y)*x
+        self.b += self.lr * (t - self.y)
+
+    def test(self, X, T):
+        correct = 0;
+        for x, t in zip(X, T):
+            if self.calc(x) == t:
+                correct += 1
+        accuracy = correct / len(T)
+        return accuracy
